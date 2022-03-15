4a4e854b95992d0afa1be789a77419aa318e8604
Jaehoon-So <jhspacelover@naver.com>
2022-03-16 01:02:44 +0900

    Create Project and add gitignore file

---
+++ AniHouse-iOS/AniHouse-iOS/AniHouse_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniHouse_iOSApp.swift
+//  AniHouse-iOS
+//
+//  Created by Jaehoon So on 2022/03/16.
+//
+
+import SwiftUI
+
+@main
+struct AniHouse_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
