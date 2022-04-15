ace309b7b33e5270d172b1834fbca4c616730dc8
Jaehoon-So <jhspacelover@naver.com>
2022-04-15 22:45:37 +0900

    add MainTabView

---
+++ AniHouse-iOS/AniHouse-iOS/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  AniHouse-iOS
+//
+//  Created by Jaehoon So on 2022/04/15.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text("this is setting view")
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
